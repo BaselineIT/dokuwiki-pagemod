68fb12f4754c5c529ebab6c8268da298165104af
Gerrit Uitslag <klapinklapin@gmail.com>
2014-11-10 00:55:04 +0100

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ */
+$lang['e_pagenotexists']       = 'De pagina [%s] bestaat niet, dus kan niet aangepast worden.';
+$lang['e_denied']              = 'Je mag deze pagina niet aanpassen, misschien ben je vergeten in te loggen?';
+$lang['e_template']            = 'De pagina die aangepast moet worden "%s" kon niet worden gelezen. Misschien bestaat de pagina niet?';
+$lang['e_failedtoparse']       = 'Bijwerken van de pagina is mislukt';
+$lang['summary']               = 'Aangepast met formulier op %s';
+$lang['pleasewait']            = 'Wacht alsjeblieft een ogenblik totdat de pagina verwerkt is.... %s of klik %s om de nieuwe versie te zien.';
