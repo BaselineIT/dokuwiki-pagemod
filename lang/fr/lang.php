69d7dfc1deebed3afbf898f3a915c5d56ea766c8
Schplurtz le Déboulonné <schplurtz@laposte.net>
2015-01-29 21:55:37 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['e_pagenotexists']       = 'Cette page [%s] n\'existe pas et ne peut être modifiée.';
+$lang['e_denied']              = 'Vous n\'êtes pas autorisés à modifier cette page, peut-être avous oublié de vous identifier.';
+$lang['e_template']            = 'Ne peut lire la page  "%s" qui sera modifiée. Peut-être qu\'elle n\'existe pas.';
+$lang['e_failedtoparse']       = 'Impossible de modifier la page.';
+$lang['pleasewait']            = 'Veuillez patienter pendant que la page traite vos résultats... %s ou cliquez sur %s pour les voir.';
