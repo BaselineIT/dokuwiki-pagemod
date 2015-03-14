ddf7d0f1f455f4f2f9dffd7b9d71d0a0150e7285
Myeongjin <aranet100@gmail.com>
2015-03-14 15:35:14 +0100

    translation update

---
+++ lang/ko/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['e_pagenotexists']       = '이 문서 [%s] 가 존재하지 않아, 수정할 수 없습니다';
+$lang['e_denied']              = '이 문서를 수정할 수 없습니다, 로그인을 잊으셨나요?';
+$lang['e_template']            = '"%s" 문서를 수정할 문서를 읽을 수 없습니다. 존재하지 않는 것이 아닐까요?';
+$lang['e_failedtoparse']       = '문서를 수정하는 데 실패했습니다';
+$lang['summary']               = '%s에서 양식에서 수정함';
+$lang['pleasewait']            = '문서에 결과를 처리하는 동안 기다려 주세요.... 보려면 %s 또는 %s를 클릭하세요';
