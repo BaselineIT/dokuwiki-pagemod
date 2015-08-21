b65987bc9b45880ece53c04ebd654dd88de356b4
Aleksandr Selivanov <alexgearbox@yandex.ru>
2015-08-21 20:50:17 +0200

    translation update

---
+++ lang/ru/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['e_denied']              = 'Вам не разрешено редактировать страницу, может быть вы забыли войти?';
