2a12cd43864914bb7be2a2e5ee6d6df18a84a497
hideaki SAWADA <chuno@live.jp>
2015-12-21 03:10:32 +0100

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author hideaki SAWADA <chuno@live.jp>
+ */
+$lang['e_pagenotexists']       = 'このページ [%s] は存在しないので変更できません。';
+$lang['e_denied']              = 'このページを変更できません。おそらくログインし忘れています。';
+$lang['e_template']            = '"%s" に変更するページを読めません。おそらく存在しません。';
+$lang['e_failedtoparse']       = 'ページの変更に失敗しました。';
+$lang['summary']               = '%s のフォームから変更されました。';
