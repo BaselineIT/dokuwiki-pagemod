b74d0a11c98db768a445c346f4b20d48533fd762
Dana <dannax3@gmx.de>
2016-02-10 14:20:47 +0100

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Dana <dannax3@gmx.de>
+ */
+$lang['e_pagenotexists']       = 'Diese Seite [%s] existiert nicht, kann nicht bearbeitet werden.';
+$lang['e_denied']              = 'Sie sind nicht berechtigt, diese Seite zu bearbeiten. Haben Sie vielleicht vergessen, sich anzumelden?';
+$lang['e_template']            = 'Konnte zu bearbeitende Seite nicht lesen "%s". Vielleicht existiert diese nicht?';
+$lang['e_failedtoparse']       = 'Bearbeiten der Seite fehlgeschlagen.';
+$lang['summary']               = 'Modifiziert durch das Formular auf %s';
+$lang['pleasewait']            = 'Bitte warten Sie, während die Seite Ihre Ergebnisse verarbeitet... %s oder klicken Sie auf %s um Sie anzuzeigen.';
