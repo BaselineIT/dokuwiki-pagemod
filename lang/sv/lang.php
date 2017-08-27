3092e138164da1449abc2b46933e6645355cfef8
Tor Härnqvist <tor@harnqvist.se>
2017-08-27 11:40:04 +0200

    translation update

---
+++ lang/sv/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['e_pagenotexists']       = 'Denna sida [%s] existerar inte, kan inte ändras';
+$lang['e_denied']              = 'Du har inte åtkomst att ändra denna sida, kanske glömde du att logga in?';
+$lang['e_template']            = 'Kunde inte läsa sidan som kommer ändras "%s". Kanske den inte finns?';
+$lang['e_failedtoparse']       = 'Kunde inte ändra sidan';
+$lang['summary']               = 'Ändrade från formuläret på %s';
+$lang['pleasewait']            = 'Var god vänta medan sida bearbetar dina resultat... %s eller klicka %s för att visa dem.';
