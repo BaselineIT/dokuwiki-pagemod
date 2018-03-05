2b0ca82eae3cf1fa8f3e27adacea3c3ddaeb77ca
philipp potocki <hans.a.muster@gmail.com>
2018-03-05 19:15:36 +0100

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author philipp potocki <hans.a.muster@gmail.com>
+ */
+$lang['e_pagenotexists']       = 'Die Seite [%s] existiert nicht; sie kann nicht geändert werden.';
+$lang['e_denied']              = 'Du darfst diese Seite nicht ändern; bist Du unter dem richtigen Namen angemeldet?';
+$lang['e_template']            = 'Die Seite "%s", die geändert werden soll, kann nicht gelesen werden. Vielleicht existiert sie gar nicht?';
+$lang['e_failedtoparse']       = 'Die Seite konnte nicht geändert werden.';
+$lang['summary']               = 'Die Seite wurde aufgrund des Formulars in %s geändert.';
+$lang['pleasewait']            = 'Bitte warte, bis die Änderungen ausgeführt sind ... %s oder Klicke auf %s, um sie zu sehen.';
