01dd27c3e65b3f29bfbad490a268db8fa1664ba9
Jürgen <hans-juergen.schuemmer@schuette.de>
2018-08-15 17:10:34 +0200

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Jürgen <hans-juergen.schuemmer@schuette.de>
+ */
+$lang['e_pagenotexists']       = 'Diese Seite [%s] existiert nicht, kann nicht geändert werden.';
+$lang['e_denied']              = 'Sie dürfen diese Seite nicht verändern! Haben Sie vergessen, sich einzuloggen?';
+$lang['e_template']            = 'Seite konnte nicht gelesen werden, die geändert wird ("%s"). Vielleicht existiert sie nicht?';
+$lang['e_failedtoparse']       = 'Die Seite konnte nicht geändert werden.';
+$lang['summary']               = 'Geändert über das Formular bei %s';
+$lang['pleasewait']            = 'Bitte warten Sie, während die Seite Ihre Ergebnisse verarbeitet...... %s oder klicken Sie auf %s, um sie zu sehen.';
