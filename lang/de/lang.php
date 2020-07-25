7030f15f03851b0bfb0769e7fd52789c1f213911
Axel Schwarzer <SchwarzerA@gmail.com>
2020-07-25 17:30:04 +0200

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Axel Schwarzer <SchwarzerA@gmail.com>
+ */
+$lang['e_pagenotexists']       = 'Diese Seite [%s] existiert nicht und kann daher nicht geändert werden.';
+$lang['e_denied']              = 'Sie dürfen diese Seite nicht ändern, haben Sie vielleicht vergessen, sich anzumelden?';
+$lang['e_template']            = 'Seite konnte nicht gelesen werden, die geändert werden soll "%s". Vielleicht existiert sie noch nicht?';
+$lang['e_failedtoparse']       = 'Konnte Seite nicht ändern!';
+$lang['summary']               = 'Geändert von der Form bei %s.';
+$lang['pleasewait']            = 'Bitte warten Sie, während die Seite Ihre Ergebnisse verarbeitet… %s oder klicken Sie auf %s, um sie anzuzeigen.';
