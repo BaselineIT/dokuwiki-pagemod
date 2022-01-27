c9d8b969450c28e057538fc6deb61c63c1035f6b
Ralf Schmitt <ralf_schmitt@yahoo.de>
2022-01-27 07:35:29 +0100

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Ralf Schmitt <ralf_schmitt@yahoo.de>
+ */
+$lang['e_pagenotexists']       = 'Diese Seite [%s] existiert nicht, kann nicht geändert werden.';
+$lang['e_denied']              = 'Du hast keine Berechtigung, diese Seite zu bearbeiten. Vielleicht hast Du vergessen, Dich anzumelden?';
+$lang['e_template']            = 'Konnte die Seite "%s", die geändert werden soll, nicht lesen. Vielleicht existiert sie nicht?';
+$lang['e_failedtoparse']       = 'Änderung der Seite fehlgeschlagen';
+$lang['summary']               = 'Geändert durch das Formular von %s';
+$lang['pleasewait']            = 'Bitte warte, während die Seite Deine Ergebnisse verarbeitet ... %s oder klicke auf %s um sie zu sehen.';
