de7f9167a638ff23d773b160583b02b298fb63f5
Ben_2003 <dsoeteber@web.de>
2022-03-12 16:20:04 +0100

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Ben_2003 <dsoeteber@web.de>
+ */
+$lang['e_pagenotexists']       = 'Diese Seite [%s] existiert nicht, kann nicht geändert werden';
+$lang['e_denied']              = 'Du bist nicht berechtigt, diese Seite zu ändern, vielleicht hast Du vergessen, dich anzumelden?';
+$lang['e_template']            = 'Seite, die geändert wird, konnte nicht gelesen werden "%s". Vielleicht existiert diese nicht?';
+$lang['e_failedtoparse']       = 'Die Seite konnte nicht geändert werden';
+$lang['summary']               = 'Geändert vom Formular bei %s';
+$lang['pleasewait']            = 'Bitte warte, während die Seite Ihre Ergebnisse verarbeitet.... %s oder klicke auf %s, um diese anzuzeigen';
