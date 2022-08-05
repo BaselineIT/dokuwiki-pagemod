a41a9d3335a75081fab07abf97d29b5f3c9dbaf2
Hella <hella.breitkopf@gmail.com>
2022-08-05 16:20:14 +0200

    translation update

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hella <hella.breitkopf@gmail.com>
+ */
+$lang['e_pagenotexists']       = 'Die Seite  [%s] gibt es nicht, kann nicht geändert werden';
+$lang['e_denied']              = 'Du darfst diese Seite nicht ändern, hast du vielleicht vergessen dich einzuloggen?';
+$lang['e_template']            = 'Konnte die Seite "%s" nicht lesen um sie zu ändern. Könnte es sein, dass es sie nicht gibt?';
+$lang['e_failedtoparse']       = 'Fehler beim Ändern der Seite';
+$lang['summary']               = 'Geändert durch das Formular unter %s';
+$lang['pleasewait']            = 'Bitte warten während die Seite deine Ergebnisse erstellt... %s oder klicke %s um die Ergebnisse zu sehen';
