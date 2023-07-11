c3edb0a4eba9e74b145de67aecc7362b4bc31b08
Sevynidd <sevynidd02@protonmail.com>
2023-07-11 13:25:32 +0200

    translation update

---
+++ lang/de/lang.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sevynidd <sevynidd02@protonmail.com>
+ */
+$lang['e_pagenotexists']       = 'Diese Seite [%s] existiert nicht, kann nicht modifiziert werden.';
+$lang['e_denied']              = 'Sie sind nicht dazu berechtigt, diese Seite zu modifizieren. Vielleicht haben Sie vergessen, sich einzuloggen?';
+$lang['e_template']            = 'Seite, welche modifiziert werden soll "%s", konnte nicht gelesen werden. Vielleicht existiert sie nicht?';
+$lang['e_failedtoparse']       = 'Seite konnte nicht modifiziert werden.';
+$lang['summary']               = 'Modifiziert von der Form bei %s';
+$lang['pleasewait']            = 'Bitte warten Sie, bis die Seite die Ergebnisse verarbeitet hat ... %s oder %s klicken, um diese zu sehen.';
